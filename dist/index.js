0e5c13f730014b0f9fa9ff7cdb70d36a646f0c12
eduardomunozalfonso <choboellobo@gmail.com>
2022-03-29 17:44:40 +0200

    express

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const bot_1 = require("./bot");
 new bot_1.default();
+const express = require("express");
+const app = express();
+app.get('/', (req, res) => {
+    res.json({ app: "working" });
+});
+app.listen(4000);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 import Bot from './bot';
 new Bot()
+
+import * as express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ app: "working"})
+})
+
+app.listen(4000)
