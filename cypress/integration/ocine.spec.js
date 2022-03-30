91e345f275e77827a1f1ad0e8c6174c09e0bb51e
eduardomunozalfonso <choboellobo@gmail.com>
2022-03-30 12:03:40 +0200

    fix

--- cypress/integration/ocine.spec.js
+++ cypress/integration/ocine.spec.js
@@ -1,77 +1,77 @@
 
 describe('Comprar entradas cine', () => {
     const { movie, time, quantity } = Cypress.env('data')
-    async function comprar3() {
-        await cy.get('input').each( async (el, index) => {
+    function comprar3() {
+        cy.get('input').each( async (el, index) => {
             switch(index) {
                 case 0: 
-                await cy.wrap(el).type("Eduardo")
+                    cy.wrap(el).type("Eduardo")
                 break;
                 case 1: 
-                await cy.wrap(el).type("Muñoz")
+                    cy.wrap(el).type("Muñoz")
                 break;
                 case 2: 
-                await cy.wrap(el).type("choboellobo@gmail.com")
+                    cy.wrap(el).type("choboellobo@gmail.com")
                 break;
                 case 3: 
-                await cy.wrap(el).type("choboellobo@gmail.com")
+                    cy.wrap(el).type("choboellobo@gmail.com")
                 break;
                 case 4: 
-                await cy.wrap(el).type("665887766")
+                    cy.wrap(el).type("665887766")
                 break;
                 case 6: 
-                await el.click()
+                    el.click()
                 break;
             }
         })
-        await cy.wait(1000);
+        cy.wait(1000);
         cy.get('button').each( async (el, index) => {
             if(index === 1){
-                await el.click()
-                await cy.wait(2000)
+                el.click()
+                cy.wait(2000)
             } 
             
         })
     } 
     async function comprar2() {
-        await cy.get('a').each( async (el, index) => {
+        cy.get('a').each( async (el, index) => {
             if( index === 0 ) {
-                await cy.wrap(el).click()
-                await cy.wait(1000)
-                await comprar3()
+                cy.wrap(el).click()
+                cy.wait(1000)
+                comprar3()
             }
         })
     }
     async function comprar()  {
-        await cy.get('title').should('to.be', 'webtpv21')
-        await cy.get('button').each( async (el, index) => {
+        cy.get('title').should('to.be', 'webtpv21')
+        cy.get('button').each( async (el, index) => {
             if( index === 1 ) {
-                await el.click()
-                await cy.wait(1000)
-                await comprar2()
+                el.click()
+                cy.wait(1000)
+                comprar2()
             }
         })
     }
-    async function cogerSitio() {
-        await cy.get('title').should('to.be', 'Distribución de la sala')
+    function cogerSitio() {
+        cy.get('title').should('to.be', 'Distribución de la sala')
         let count = 0;
-        await cy.get('.botonnormal').each( async (el, i) => {
+        cy.get('.botonnormal').each( async (el, i) => {
             if( i < quantity) {
-                await cy.wrap(el).click()
-                await cy.wait(1000)
+                cy.wrap(el).click()
+                cy.wait(1000)
                 count++
                 if( quantity == count) {
-                    await cy.get('#compra input').click()
-                    await cy.wait(1000)
-                    await comprar()
+                    cy.get('#compra input').click()
+                    cy.wait(1000)
+                    comprar()
                 }
             }
         })
     }
     it('Comprar entradas', async () => {
         let click = false;
-        await cy.visit('https://www.ocinerioshopping.es/')
-        await cy.get('.pelis-grid .peli-item').each( async (elem, index) => {
+        cy.visit('https://www.ocinerioshopping.es/')
+        cy.get('.pelis-grid .peli-item').each(  (elem, index) => {
             
             if( elem.css('display') === 'block') {
                 
@@ -80,10 +80,10 @@ describe('Comprar entradas cine', () => {
                         if(e.innerText.trim() ===  time ) {
                            if( !click ) {
                             click = true;
-                            await e.click()
-                            await cy.log("Click element")
-                            await cy.wait(2000)
-                            await cogerSitio()
+                             e.click()
+                             cy.log("Click element")
+                             cy.wait(3000)
+                             cogerSitio()
                            } 
                         } 
                     })
